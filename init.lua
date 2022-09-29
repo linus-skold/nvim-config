0ce7de38d7a2d949df872f140d01b430738d8929
Linus Sköld <originaldecode@outlook.com>
2022-09-29 10:46:34 +0200

    moved lua files to lua, fixed init

--- init.lua
+++ init.lua
@@ -1,15 +1,19 @@
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.syntax = "on"
+-- vim.opt.colorscheme = "one-monokai"
 
 if jit.os == 'Windows' then
     vim.cmd("source ~/AppData/Local/nvim/user/plugins.vim")
     vim.cmd("source ~/AppData/Local/nvim/user/keymap.vim")
 elseif jit.os == 'OSX' then
-    require("user/plugins")
-    vim.cmd("source ~/.config/nvim/user/keymap.vim")
+    require('/user/plugins')
+    vim.cmd("source ~/.config/nvim/lua/user/keymap.vim")
 end
 
-vim.opt.number = true
 vim.keymap.set("i", "<C-p>", "CtrlP", {desc = "bruh"})
 
+vim.api.nvim_command [[colorscheme one-monokai"]]
 -- let g:ctrlp_map = '<c-p>'
 -- let g:ctrlp_cmd = 'CtrlP'
 -- let g:LanguageClient_serverCommands = { 'rust':['rust-analyzer'] }
@@ -62,8 +66,11 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.cmd("source ~/AppData/Local/nvim/init.vim")
-
+if jit.os == 'Windows' then
+    vim.cmd("source ~/AppData/Local/nvim/init.vim")
+elseif jit.os == 'OSX' then
+    -- vim.cmd("source init.vim")
+end
 
 -- vim.cmd("colorscheme one-monokai")
 
--- init.vim
+++
@@ -1,8 +0,0 @@
-syntax "on"
-colorscheme one-monokai
-set termguicolors 
-"highlight Normal ctermbg=NONE guibg=NONE
-"highlight NonText ctermbg=NONE guibg=NONE
-"highlight SignColumn ctermbg=NONE guibg=NONE
-set shm+=I
-
