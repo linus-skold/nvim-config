7722aae20a51b69b3c6cdd856d4554daca405ed3
Linus <contact@linusskold.com>
2025-10-21 00:26:42 +0200

    feat: Added folke/which-key to config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -326,6 +326,20 @@ local plugins = {
 		},
 	},
     {
+        "folke/which-key.nvim", 
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>?",
+                function() 
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)"
+            }
+        }
+    },
+    {
     "atiladefreitas/dooing",
     config = function()
         require("dooing").setup({
