215646128a82375bc9ce1df580ee4ef213b942f8
Linus <contact@linusskold.com>
2025-11-03 00:36:23 +0100

    feat(lsp): Added inline lsp diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,35 @@
 return {
 	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "LspAttach",
+		opts = {
+			options = {
+				multilines = {
+					enabled = true,
+				},
+			},
+		},
+		config = function(_, opts)
+			local tid = require("tiny-inline-diagnostic")
+			tid.setup(opts)
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
+
+			vim.api.nvim_create_user_command("TinyInlineDiagnosticMultilinesToggle", function()
+				local current = tid.config.options.multilines.enabled
+				tid.config.options.multilines.enabled = not current
+			end, {})
+		end,
+		keys = {
+			{
+				"<leader>tm",
+				"<cmd>TinyInlineDiagnosticMultilinesToggle<CR>",
+				desc = "Toggle Tiny Inline Diagnostic Multilines",
+			},
+		},
+	},
+	{
 		"williamboman/mason.nvim", -- lsp installer
 		opts = {
 			registries = {
@@ -10,6 +40,7 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "nvimdev/lspsaga.nvim" },
 		lazy = true,
 		event = { "BufReadPre", "BufNewFile" },
 	},
