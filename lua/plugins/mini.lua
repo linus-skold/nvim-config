a3fed403b25d6e755bbdb68ae9c60413b649a65b
linus-skold <contact@linusskold.com>
2025-11-03 19:10:57 +0100

    feat(plugin): Implemented mini.sessions, added command to create a new session

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,4 @@
+
 return {
 	{
 		"nvim-mini/mini.completion",
@@ -23,4 +24,48 @@ return {
 			},
 		},
 	},
+    {
+        "nvim-mini/mini.sessions",
+        dependencies = { "folke/snacks.nvim" },
+        version = "*",
+        event = "VeryLazy",
+        opts = {
+            autoread = false,
+            autowrite = true,
+            directory = function()
+                local git_root = vim.fn.finddir(".git", ".;")
+                if git_root ~= "" then
+                    return vim.fn.fnamemodify(git_root, ":h") .. "/.git/nvim-sessions"
+                end
+                return vim.fn.stdpath("data") .. "/sessions"
+            end,
+        },
+        keys = {
+            { "<leader>cS", function() 
+                Snacks.input({
+                    prompt = "Session name: ",
+                }, function(input)
+                    if input then
+                        require("mini.sessions").write(input)
+                    end
+                end)
+            end, desc = "Create Session" }  
+        },
+        config = function(_, opts)
+            require("mini.sessions").setup(opts)
+            -- Delete initial no-name buffer after session is manually loaded
+            vim.api.nvim_create_autocmd("SessionLoadPost", {
+                callback = function()
+                    vim.defer_fn(function()
+                        local bufs = vim.api.nvim_list_bufs()
+                        for _, bufnr in ipairs(bufs) do
+                            if vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_buf_get_name(bufnr) == "" and vim.bo[bufnr].buftype == "" and not vim.bo[bufnr].modified then
+                                vim.api.nvim_buf_delete(bufnr, { force = true })
+                            end
+                        end
+                    end, 10)
+                end,
+            })
+        end,
+    }
 }
