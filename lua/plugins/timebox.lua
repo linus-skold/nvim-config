8e96ee14a5e005a1b3fe06673a0d13786efb5928
Linus <contact@linusskold.com>
2025-11-10 23:28:22 +0100

    chore: added a dev mode for timebox

--- lua/plugins/timebox.lua
+++ lua/plugins/timebox.lua
@@ -1,10 +1,15 @@
 return {
 	"linus-skold/timebox.nvim",
+	dev = true,
+    dir = "~/Development/timebox.nvim",
 	dependencies = { "folke/snacks.nvim" },
 	opts = {
 		duration = {
 			work = 1 * 60,
 			coffee = 1 * 60,
 		},
-	},
+        win = {
+            width = 120,
+        },
+    },
 }
